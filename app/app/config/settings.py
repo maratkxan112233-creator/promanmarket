b3fa0a83df3ba719473bd605c1cee60b4f68c592
maratkxan3-oss <maratkxan3@gmail.com>
2026-06-02 08:58:14 +0500

    Update settings.py

--- app/app/config/settings.py
+++ app/app/config/settings.py
@@ -1,11 +1,12 @@
+from typing import Optional
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
-    DATABASE_URL: str
-    REDIS_URL: str
     OWNER_ID: int
+    DATABASE_URL: Optional[str] = None
+    REDIS_URL: Optional[str] = None
 
     model_config = SettingsConfigDict(
         env_file=".env",
